8dc3d003026c56ca17bbd82c589a42b2793e1e07
Abhineetsahay <abhineetsahay@gmail.com>
2025-02-14 15:04:53 +0530

    add Inspiration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Footer from "./components/Footer";
+import Inspiration from "./components/Inspiration";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="h-full w-full absolute top-0 left-0 flex flex-col">
       <Navbar />
+      <Inspiration/>
       <Footer />
     </div>
   )
---
+++ src/components/Inspiration.tsx
@@ -0,0 +1,13 @@
+import AchyutaSamanta from "../ASSETS/Achyuta-Samanta-KIIT-University-1200x661.jpg"
+
+const Inspiration=()=>{
+    return (
+        <div className="m-10">
+            <div>
+            <img src={AchyutaSamanta} className="h-100 w-100 object-cover"/>
+            </div>
+        </div>
+    )
+}
+
+export default Inspiration;
