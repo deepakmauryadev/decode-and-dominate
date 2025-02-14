5a345b1572cd6408dae3991e4b0eeea222928cbf
Deepak Maurya <dee221403@gmail.com>
2025-02-14 15:32:54 +0530

    Initial commit

--- src/components/Accreditation.tsx
+++ src/components/Accreditation.tsx
@@ -5,16 +5,25 @@ import Accdn4 from "../ASSETS/accdn4.webp";
 
 const cards = [
   { image: Accdn1 },
-  { image: Accdn1 },
-  { image: Accdn1 },
-  { image: Accdn1 },
-]
+  { image: Accdn2 },
+  { image: Accdn3 },
+  { image: Accdn4 },
+];
 
 export default function Accreditation() {
   return (
-    <div className="flex gap-5 bg-zinc-200 px-40 py-10">
+    <div className="flex flex-col gap-5 bg-zinc-200 px-40 py-10">
       <h1 className="text-3xl font-bold">Accreditation</h1>
-
+      <div className="flex gap-4">
+        {cards.map((card) => (
+          <img
+            src={card.image}
+            alt="Accreditation"
+            key={card.image}
+            className="w-72 h-auto"
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
